b2f38e08ace337983155a0ab8fc485b5aadbd48e
gabrielavl298 <gaby.12leos@gmail.com>
2021-10-12 22:33:29 -0500

    Login screen

--- constants/Theme.js
+++ constants/Theme.js
@@ -1,8 +1,10 @@
 export default{
     COLORS: {
         DEFAULT: '#DCDCDC',
-        PRIMARY: '#9C26B0',
-        LABEL: '#FE2472',
+        PRIMARY: '#6E1B7D',
+        SECONDARY: '#BB69C9',
+        LABEL1: '#E38DA4',
+        LABEL2: '#FE2472',
         INFO: '#00BCD4',
         ERROR: '#F44336',
         SUCCESS: '#4CAF50',
@@ -11,7 +13,8 @@ export default{
         INPUT: '#DCDCDC',
         ACTIVE: '#9C26B0',
         BUTTON_COLOR: '#9C26B0',
-        PLACEHOLDER: '#9FA5AA',
+        PLACEHOLDER1: '#884C93',
+        PLACEHOLDER2: '#9FA5AA',
         SWITCH_ON: '#9C26B0',
         SWITCH_OFF: '#D4D9DD',
         GRADIENT_START: '#6B24AA',
@@ -19,9 +22,18 @@ export default{
         PRICE_COLOR: '#EAD5FB',
         BORDER_COLOR: '#E7E7E7',
         BLOCK: '#E7E7E7',
-        ICON: '#4A4A4A',
+        ICON1: '#852196',
+        ICON2: '#4A4A4A',
+        OTHER:{
+          GOOGLEBTN: '#DD4B39'
+        }
       },
       SIZES: {
         BLOCK_SHADOW_RADIUS: 2,
+        TEXT: {
+          TITLE: 25,
+          SUBTITLE:20,
+          NORMAL:15
+        }
       }
 };
---
+++ screens/LoginScreen.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {colors, Input, Button} from 'react-native-elements'
+
+import Theme from '../constants/Theme'
+import { color } from 'react-native-elements/dist/helpers';
+
+const LoginScreen = () => {
+    return (
+        <View style = {styles.container}>
+            <View style= {{flex: 1, alignItems: 'center', justifyContent: 'center'}} >
+                <Image source= {require('../assets/images/icon.png')} style= {styles.logo}/>
+            </View>
+            <View style= {{flex: 2, alignItems: 'center', paddingTop: 5}}>
+                <Text style={styles.titleTxt}>Login</Text>
+                <View style={{width: '85%'}}>
+                    <View>
+                        <Input placeholder = 'email@address.com' 
+                            color={Theme.COLORS.SECONDARY}
+                            placeholderTextColor={Theme.COLORS.PLACEHOLDER1}
+                            labelStyle= {{color:  Theme.COLORS.LABEL1}}
+                            label = 'Your email address'
+                            leftIcon={ <Icon name='at' size={24} 
+                                color= {Theme.COLORS.ICON1}
+                            />}
+                            autoCompleteType = 'off'
+                        />
+                    </View>
+                    <View>
+                        <Input placeholder = 'Password' 
+                            color={Theme.COLORS.SECONDARY}
+                            placeholderTextColor={Theme.COLORS.PLACEHOLDER1}
+                            labelStyle= {{color:  Theme.COLORS.LABEL1}}
+                            label = 'Your password'  
+                            leftIcon={ <Icon name='key' size={24} 
+                            color= {Theme.COLORS.ICON1}
+                            />}
+
+                            secureTextEntry={true}
+                            
+                            
+                        />
+                    </View>
+                    <View style={{alignItems: 'center'}}>
+                        <View style={{width:'70%'}}>
+                            <Button
+                                title = 'Log in'
+                                buttonStyle = {{backgroundColor: Theme.COLORS.PRIMARY}}
+                            />
+                        </View>
+                    </View>
+                    <View style={{alignItems: 'center'}}>
+                        <View style={{width:'70%', paddingTop:10}}>
+                            <Button
+                                title = 'Log in with google'
+                                buttonStyle = {{backgroundColor: Theme.COLORS.OTHER.GOOGLEBTN}}
+                                icon = {<Icon name='google' size = {24} color='white' style={{marginRight:7}}/>}
+                            />
+                        </View>
+                    </View>
+                    <View style= {{paddingTop: 5, alignItems: 'center'}}>
+                        <Text style={styles.textStyle}> New here? <Text style={styles.linkText} onPress={() => {}}> create an account</Text></Text>
+                    </View>
+                </View>
+            </View>
+            
+        </View>
+    )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: Theme.COLORS.DEFAULT,
+        paddingTop: 10
+        
+    },
+
+    logo:{
+        width: '65%',
+        height: '61.5%',
+        paddingTop: '50%'
+    },
+
+    titleTxt:{
+        fontSize: Theme.SIZES.TEXT.TITLE,
+        color: Theme.COLORS.PRIMARY,
+        fontWeight: 'bold'
+    },
+
+    textStyle:{
+        color: Theme.COLORS.SECONDARY
+    },
+    linkText:{
+        color: Theme.COLORS.SECONDARY,
+        textDecorationLine: 'underline'
+    }
+})
